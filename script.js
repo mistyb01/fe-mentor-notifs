b0c729d4df896ae2223026231d1defccb1a07798
Misty Becker <mistyb01@uw.edu>
2022-10-13 12:11:27 -0700

    able to mark all as read

---
+++ script.js
@@ -0,0 +1,16 @@
+const markAllAsReadLink = document.getElementById('mark-read');
+markAllAsReadLink.addEventListener('click', markAllAsRead);
+function markAllAsRead() {
+    const notifCards = document.getElementsByClassName('notif');
+    Array.from(notifCards).forEach(card => {
+        if (card.classList.contains('new')) {
+            card.classList.remove('new');
+        }
+    });
+    const dots = document.getElementsByClassName('dot');
+    Array.from(dots).forEach(dot => dot.parentNode.removeChild(dot));
+    const notifNumber = document.getElementById('notif-number');
+    console.log(notifNumber);
+    notifNumber.parentNode.removeChild(notifNumber);
+}
+
